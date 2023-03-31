dcdd0bf2547be204903429bebeca5dcca34e524a
Megan Cindric <megancindric@gmail.com>
2023-03-31 16:44:07 -0500

    📦 NEW: Created Favorite & Review model & schema

---
+++ backend/resources/userreviews.py
@@ -0,0 +1,5 @@
+from flask import request
+from flask_jwt_extended import jwt_required, get_jwt_identity, verify_jwt_in_request
+from flask_restful import Resource
+from database.models import db, Review
+from database.schemas import review_schema, reviews_schema
