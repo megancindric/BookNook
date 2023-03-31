3bcb0efd689bdc4aaa2648ee9fb15c00a35d4256
Megan Cindric <megancindric@gmail.com>
2023-03-31 17:04:56 -0500

    📦 NEW: Added GET & POST resources for favorites & reviews

--- backend/resources/userfavorites.py
+++ backend/resources/userfavorites.py
@@ -3,3 +3,21 @@ from flask_jwt_extended import jwt_required, get_jwt_identity, verify_jwt_in_req
 from flask_restful import Resource
 from database.models import db, Favorite
 from database.schemas import favorite_schema, favorites_schema
+
+
+class UserFavoriteResource(Resource):
+    @jwt_required()
+    def get(self):
+        user_id = get_jwt_identity()
+        user_favorites = Favorite.query.filter_by(user_id=user_id)
+        return user_favorites.dump(user_favorites), 200
+    
+    @jwt_required()
+    def post(self):
+        user_id = get_jwt_identity()
+        form_data = request.get_json()
+        new_favorite = favorite_schema.load(form_data)
+        new_favorite.user_id = user_id
+        db.session.add(new_favorite)
+        db.session.commit()
+        return favorite_schema.dump(new_favorite), 201
--- backend/resources/userreviews.py
+++ backend/resources/userreviews.py
@@ -3,3 +3,14 @@ from flask_jwt_extended import jwt_required, get_jwt_identity, verify_jwt_in_req
 from flask_restful import Resource
 from database.models import db, Review
 from database.schemas import review_schema, reviews_schema
+
+class UserReviewResource(Resource):
+    @jwt_required()
+    def post(self):
+        user_id = get_jwt_identity()
+        form_data = request.get_json()
+        new_review = review_schema.load(form_data)
+        new_review.user_id = user_id
+        db.session.add(new_review)
+        db.session.commit()
+        return review_schema.dump(new_review), 201
