3515ef1c6644ffe5386b54392472da8da168c318
Megan Cindric <megancindric@gmail.com>
2023-04-04 18:49:02 -0500

    🐛 FIX: Adjusted GetBookInformation resource to not require JWT

--- backend/resources/bookinformation.py
+++ backend/resources/bookinformation.py
@@ -5,21 +5,24 @@ from database.models import db, Review, Favorite
 from database.schemas import review_schema, reviews_schema
 
 class GetBookInformation(Resource):
-    @jwt_required()
     def get(self):
         custom_response = {}
-        user_id = get_jwt_identity()
         book_id = request.args.get("book_id")
         book_reviews = Review.query.filter_by(book_id=book_id)
         custom_response["reviews"] = reviews_schema.dump(book_reviews)
         avg_review = sum(review.rating for review in book_reviews) / len(custom_response["reviews"])
         custom_response["avg_review"] = avg_review
-        if user_id:
+
+        try:
+            verify_jwt_in_request()
+            user_id = get_jwt_identity()
             matching_favorite = Favorite.query.filter_by(user_id=user_id, book_id = book_id).first()
             if matching_favorite:
                 custom_response["is_favorite"] = True
-        else: 
-            custom_response["is_favorite"] = False
+            else: 
+                custom_response["is_favorite"] = False
+        except:
+            pass
         return custom_response, 200
     # All reviews from DB related to query param book ID
     # Average rating
