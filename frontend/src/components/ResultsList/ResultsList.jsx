8ce6ba603409fa9e7c6ebf9717aaae6ec7ec36fc
Megan Cindric <megancindric@gmail.com>
2023-04-04 20:39:04 -0500

    👌 IMPROVE: Mapping searched book response to html table

--- frontend/src/components/ResultsList/ResultsList.jsx
+++ frontend/src/components/ResultsList/ResultsList.jsx
@@ -2,7 +2,29 @@ import React, { useState } from "react";
 
 const ResultsList = ({ searchResults }) => {
   console.log(searchResults);
-  return <div>results list</div>;
+  return (
+    <table>
+      <thead>
+        <tr>
+          <th>Title</th>
+          <th>Authors</th>
+          <th>Publisher</th>
+          <th>Publish Date</th>
+        </tr>
+      </thead>
+      <tbody>
+        {searchResults.map((book) => (
+          <tr key={book.id}>
+            <td>{book.volumeInfo.title}</td>
+            <td>{book.volumeInfo.authors}</td>
+
+            <td>{book.volumeInfo.publisher}</td>
+            <td>{book.volumeInfo.publishedDate}</td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
 };
 
 export default ResultsList;
