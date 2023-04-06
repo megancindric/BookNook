c9834a881d644ef4d14c926783ce8cb6c2759d08
Megan Cindric <megancindric@gmail.com>
2023-04-06 10:46:17 -0500

    🐛 FIX: Added reviews back to custom response, even if empty

--- backend/resources/bookinformation.py
+++ backend/resources/bookinformation.py
@@ -10,6 +10,7 @@ class GetBookInformation(Resource):
         book_id = request.args.get("book_id")
         book_reviews = Review.query.filter_by(book_id=book_id)
         book_reviews = reviews_schema.dump(book_reviews)
+        custom_response["reviews"] = book_reviews
         if len(book_reviews) > 0:
             custom_response["avg_review"] = sum(review.rating for review in book_reviews) / len(book_reviews)
         else:
