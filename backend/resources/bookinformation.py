72502cd1b8240d6b5af84d1dfaa8456fb8c8dc27
Megan Cindric <megancindric@gmail.com>
2023-04-06 10:45:33 -0500

    🐛 FIX: Corrected error calculating avg review if no reviews listed for book

--- backend/resources/bookinformation.py
+++ backend/resources/bookinformation.py
@@ -9,9 +9,11 @@ class GetBookInformation(Resource):
         custom_response = {}
         book_id = request.args.get("book_id")
         book_reviews = Review.query.filter_by(book_id=book_id)
-        custom_response["reviews"] = reviews_schema.dump(book_reviews)
-        avg_review = sum(review.rating for review in book_reviews) / len(custom_response["reviews"])
-        custom_response["avg_review"] = avg_review
+        book_reviews = reviews_schema.dump(book_reviews)
+        if len(book_reviews) > 0:
+            custom_response["avg_review"] = sum(review.rating for review in book_reviews) / len(book_reviews)
+        else:
+            custom_response["avg_review"] = 0
 
         try:
             verify_jwt_in_request()
