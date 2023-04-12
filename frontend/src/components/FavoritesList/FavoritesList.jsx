6e2d9587c5075d124f7463efbeddcf26cefc06db
Megan Cindric <megancindric@gmail.com>
2023-04-12 15:47:02 -0500

    👌 IMPROVE: Users can now click favorite to be taken to book detail page

--- frontend/src/components/FavoritesList/FavoritesList.jsx
+++ frontend/src/components/FavoritesList/FavoritesList.jsx
@@ -1,13 +1,16 @@
 import React, { useState } from "react";
 import FavoriteCard from "../FavoriteCard/FavoriteCard";
 import LoadingSpinner from "../LoadingSpinner/LoadingSpinner";
+import { Link } from "react-router-dom";
 const FavoritesList = ({ userFavorites }) => {
   console.log(userFavorites);
   return (
     <div className="flex flex-row flex-wrap gap-24">
       {userFavorites ? (
         userFavorites.map((book, index) => (
-          <FavoriteCard book={book} key={index} />
+          <Link key={index} to={`/bookdetails/${book.book_id}`}>
+            <FavoriteCard book={book} />
+          </Link>
         ))
       ) : (
         <LoadingSpinner />
